e3fbab8c2a235cde96ece6ea2fe8f80a1e418b14
ziwang0411 <ziwang0411@gmail.com>
2020-12-31 20:00:24 -0800

    ready to deploy

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV !=='production') {
     require('dotenv').config();
 }
-
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -10,23 +9,25 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const session = require('express-session')
+const MongoDBStore = require('connect-mongo')(session);
 const flash = require('connect-flash')
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
-// const helmet = require("helmet");
+const helmet = require("helmet");
 
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/review');
-const { isLoggedIn } = require('./middleware');
 const { render } = require('ejs');
 // const Joi = require('joi')
 //for error handling
-
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
+const dbUrl = process.env.DB_URL ||'mongodb://localhost:27017/yelp-camp';
+const secret = process.env.SECRET||'thisisanothersecreeeettteetetet';
+// 
+mongoose.connect(dbUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
     .then(() => {
         console.log("MONGO CONNECTION OPEN!!!")
     })
@@ -40,9 +41,23 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true })); //parse the req.body
 app.use(methodOverride('_method')); //on every single request, use the function(methodoverride)
+
+
+// store session on mongo
+const store = new MongoDBStore({
+    url:dbUrl,
+    secret,
+    touchAfter:24*60*60
+});
+
+store.on("error", function(e) {
+    console.log("SESSION STORE ERROR", e);
+})
+
 const sessionConfig = {
+    store,
     name: 'DODODO',
-    secret: 'SECRRRRRRRRRRRRRREEEEEEEEEEEEETTTTTTTTTTTT',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -55,11 +70,52 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 app.use(mongoSanitize());
-// app.use(
-//     helmet({
-//       contentSecurityPolicy: false,
-//     })
-//   );
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dmcfezt27/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+                "https://source.unsplash.com/"
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
