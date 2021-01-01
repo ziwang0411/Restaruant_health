f07562551c28a1f621d5fae932a40e52f76c8aca
ziwang0411 <38840641+ziwang0411@users.noreply.github.com>
2020-12-31 20:23:11 -0800

    added start script in package.JSON

--- app.js
+++ app.js
@@ -160,6 +160,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 //listen
-app.listen(3000, () => {
-    console.log("Serving on Port 3000")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on Port ${port}`)
 })
