e3fdc4866b74c90a33314646a26a799fe348e7e6
ziwang0411 <ziwang0411@gmail.com>
2021-04-27 21:02:43 -0700

    it's a restaruant now

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const { isLoggedIn,validateCampground,isAuthor} = require('../middleware');
+const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds')
-var multer  = require('multer');
-const {storage} = require('../cloudinary')
+var multer = require('multer');
+const { storage } = require('../cloudinary')
 var upload = multer({ storage });
-const Campground = require('../models/campground');
 
 
 //check if user is logged in 
@@ -14,8 +13,8 @@ const Campground = require('../models/campground');
 //show
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn,  upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
-    
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 router.route('/:id')
@@ -25,6 +24,6 @@ router.route('/:id')
 
 //create a campground
 
-router.get('/:id/edit', isLoggedIn,isAuthor, catchAsync(campgrounds.renderEditForm))
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
 module.exports = router;
